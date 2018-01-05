cd36beb7f12ffa3c9a0a4ac2fef0d68c04b893f4
brtlvrs <bart@bartlievers.nl>
2018-01-05 09:45:05 +0100

    some small changes

--- create-VMConfig/Create-VMConfig.ps1
+++ create-VMConfig/Create-VMConfig.ps1
@@ -47,143 +47,47 @@ Begin{
 #region Functions
 
     function exit-script {
-    <#
-    .DESCRIPTION
-        function to exit script clean.
-    #>
+        <#
+        .DESCRIPTION
+            function to exit script clean.
+        #>
 
-    [CmdletBinding()]
-    Param()
-    #-- disconnect vCenter connections (if there are any)
-    if (((Get-Variable -Scope global -Name DefaultVIServers -ErrorAction SilentlyContinue ).value) -and ($P.noDisconnectOnExit -eq $false)) {
-        Disconnect-VIServer -server * -Confirm:$false
-    }
-    #-- clock time and say bye bye
-    $ts_end=get-date
-    if ($NormalExit) {write-host "Script ends normal."}
-    else {write-host "Scripts exit is premature." -ForegroundColor Yellow}
-    write-host ("Runtime script: {0:hh}:{0:mm}:{0:ss}" -f ($ts_end- $TS_start)  )
-    read-host "End script. bye bye ([Enter] to quit.)"
-    exit
+        [CmdletBinding()]
+        Param()
+
+        #-- disconnect vCenter connections (if there are any)
+        if (((Get-Variable -Scope global -Name DefaultVIServers -ErrorAction SilentlyContinue ).value) -and ($P.noDisconnectOnExit -eq $false)) {
+            Disconnect-VIServer -server * -Confirm:$false
+        }
+
+        #-- clock time and say bye bye
+        $ts_end=get-date
+        if ($NormalExit) {write-host "Script ends normal."}
+        else {write-host "Scripts exit is premature." -ForegroundColor Yellow}
+        write-host ("Runtime script: {0:hh}:{0:mm}:{0:ss}" -f ($ts_end- $TS_start)  )
+        read-host "End script. bye bye ([Enter] to quit.)"
+        exit
     }
 
     function import-PowerCLi {
     <#
     .SYNOPSIS
-       Loading of all VMware modules and power snapins
-    .DESCRIPTION
-
-    .EXAMPLE
-        One or more examples for how to use this script
-    .NOTES
-        File Name          : import-PowerCLI.ps1
-        Author             : Bart Lievers
-        Prerequisite       : <Preruiqisites like
-                             Min. PowerShell version : 2.0
-                             PS Modules and version :
-                                PowerCLI - 5.5
-        Last Edit          : BL - 22-11-2016
+       Loading of all VMware modules
     #>
-    [CmdletBinding()]
-
-    Param(
-    )
-
-    Begin{
-
-    }
-
-    Process{
-        #-- make up inventory and check PowerCLI installation
-        $RegisteredModules=Get-Module -Name vmware* -ListAvailable -ErrorAction ignore | % {$_.Name}
-        $RegisteredSnapins=get-pssnapin -Registered vmware* -ErrorAction Ignore | %{$_.name}
-        if (($RegisteredModules.Count -eq 0 ) -and ($RegisteredSnapins.count -eq 0 )) {
-            #-- PowerCLI is not installed
-            if ($log) {$log.warning("Cannot load PowerCLI, no VMware Powercli Modules and/or Snapins found.")}
-            else {
-            write-warning "Cannot load PowerCLI, no VMware Powercli Modules and/or Snapins found."}
-            #-- exit function
-            return $false
-        }
-
-        #-- load modules
-        if ($RegisteredModules) {
-            #-- make inventory of already loaded VMware modules
-            $loaded = Get-Module -Name vmware* -ErrorAction Ignore | % {$_.Name}
-            #-- make inventory of available VMware modules
-            $registered = Get-Module -Name vmware* -ListAvailable -ErrorAction Ignore | % {$_.Name}
-            #-- determine which modules needs to be loaded, and import them.
-            $notLoaded = $registered | ? {$loaded -notcontains $_}
-
-            foreach ($module in $registered) {
-                if ($loaded -notcontains $module) {
-                    Import-Module $module
-                }
-            }
-        }
-
-        #-- load Snapins
-        if ($RegisteredSnapins) {      
-            #-- Exlude loaded modules from additional snappins to load
-            $snapinList=Compare-Object -ReferenceObject $RegisteredModules -DifferenceObject $RegisteredSnapins | ?{$_.sideindicator -eq "=>"} | %{$_.inputobject}
-            #-- Make inventory of loaded VMware Snapins
-            $loaded = Get-PSSnapin -Name $snapinList -ErrorAction Ignore | % {$_.Name}
-            #-- Make inventory of VMware Snapins that are registered
-            $registered = Get-PSSnapin -Name $snapinList -Registered -ErrorAction Ignore  | % {$_.Name}
-            #-- determine which snapins needs to loaded, and import them.
-            $notLoaded = $registered | ? {$loaded -notcontains $_}
-
-            foreach ($snapin in $registered) {
-                if ($loaded -notcontains $snapin) {
-                    Add-PSSnapin $snapin
-                }
-            }
-        }
-        #-- show loaded vmware modules and snapins
-        if ($RegisteredModules) {get-module -Name vmware* | select name,version,@{N="type";E={"module"}} | ft -AutoSize}
-          if ($RegisteredSnapins) {get-pssnapin -Name vmware* | select name,version,@{N="type";E={"snapin"}} | ft -AutoSize}
-
-    }
-
-    End{
-
-    }
-
-    }
-
-    function Ask-yesNo {
         [CmdletBinding()]
-        param(
-            [string]$question,
-            [scriptblock]$codeY,
-            [scriptblock]$codeN
-        )
-
-        Do {
-            $answer=read-host ($question + "? [yN]")
-            Switch -Regex ($answer) {
-                "\A(Y|y)\Z" {
-                    #-- input is only a y or a Y
-                    $loopDone=$true #-- we had a valid answer, so exit loop
-                    & $codeY
-                    break
-                }
-                "\A(n|N|)\Z" {
-                    #-- input is only a n or N or nothing. \A = start of line \Z is end of the line==> \A\Z = nothing returned = empty answer
-                    $loopdone=$true
-                    & $codeN
-                    break
-                }
-                default {
-                    #-- the default action, no valid input
-                    $loopdone=$false
-                    write-host "Unknown answer."
-                }
+        Param()
+        Process{
+            #-- check if PowerCLI is installed
+            if ((get-module -ListAvailable vmware*).count -lt 1) {
+                write-host "VMware PowerCLI modules not found."
+                exit-script
             }
-        } until ($loopDone)
-        Return $answer
-
+            #-- Load all modules
+            get-module -ListAvailable vmware* | Import-Module
+        }
     }
+    
+    #endregion
 }
 
 End{
